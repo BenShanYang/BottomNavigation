b3d62f1b40dbf8897b40216750ca2b9180c3c739
BenShanYang <1805197091@qq.com>
2021-03-07 13:21:51 +0800

    创建首页底部导航栏控件项目

---
+++ app/src/main/java/com/yk/bottomnavigation/MainActivity.java
@@ -0,0 +1,75 @@
+package com.yk.bottomnavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.yk.navigation.SpaceItem;
+import com.yk.navigation.SpaceNavigationView;
+import com.yk.navigation.SpaceOnClickListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SpaceNavigationView navigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        navigation = findViewById(R.id.navigation);
+
+        initNavigationBar(savedInstanceState);
+    }
+
+    /**
+     * 初始化自定义导航栏
+     * https://github.com/BenShanYang/Space-Navigation-View
+     *
+     * @param savedInstanceState
+     */
+    private void initNavigationBar(Bundle savedInstanceState) {
+        navigation.initWithSaveInstanceState(savedInstanceState);
+        navigation.addSpaceItem(new SpaceItem("HOME", R.drawable.ic_home_normal));
+        navigation.addSpaceItem(new SpaceItem("MY", R.drawable.ic_my_normal));
+        navigation.setSpaceOnClickListener(new SpaceOnClickListener() {
+            @Override
+            public void onCentreButtonClick() {
+                showToast("点击了中间的按钮");
+            }
+
+            @Override
+            public void onItemClick(int itemIndex, String itemName) {
+                if (itemIndex == 0) {
+                    showToast("点击了左边的按钮");
+                } else if (itemIndex == 1) {
+                    showToast("点击了右边的按钮");
+                }
+            }
+
+            @Override
+            public void onItemReselected(int itemIndex, String itemName) {
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        navigation.onSaveInstanceState(outState);
+    }
+
+    /**
+     * 弹出提示
+     *
+     * @param message 提示的信息
+     */
+    public void showToast(CharSequence message) {
+        if (message != null) {
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
