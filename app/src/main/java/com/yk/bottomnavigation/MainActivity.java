bc0e9060300417a78c0aeca489f932795ab32f7c
BenShanYang <1805197091@qq.com>
2021-03-07 13:26:13 +0800

    创建首页底部导航栏控件项目

--- app/src/main/java/com/yk/bottomnavigation/MainActivity.java
+++ app/src/main/java/com/yk/bottomnavigation/MainActivity.java
@@ -18,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         navigation = findViewById(R.id.navigation);
-
         initNavigationBar(savedInstanceState);
     }
 
